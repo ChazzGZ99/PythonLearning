9a236c5ebe35e085a6ebe3af922e0bbcec65922a
ChazzGZ99 <119078899+ChazzGZ99@users.noreply.github.com>
2023-10-06 11:39:23 -0600

    Add files via upload

---
+++ 00_hello.py
@@ -0,0 +1,12 @@
+# Esto es un hola Mundo
+#Esto es un comentario
+
+print("hola mundo")
+
+
+#Como consultar un tipo de dato
+print(type('Soy un dato str')) #tipo de dato string
+print(type(5)) #Tipo de dato int
+print(type(1.5)) #Tipo de dato float
+print(type(True)) #Tipo de dato bool
+print(type(1 + 3j)) #Tipo de dato complejo
---
+++ 01_variables.py
@@ -0,0 +1,16 @@
+#Variables
+
+my_variable = "Mi variable de String"
+print(my_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = False
+print(my_bool_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_variable)
+print(type(my_int_to_str_variable))
+
+print(my_variable, str(my_int_variable) , my_bool_variable)
